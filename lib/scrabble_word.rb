a6724d6cb3bae257c7292bcdb9ac96de2cee112a
marinakiri <marina.kir@hotmail.fr>
2017-07-19 23:53:06 +0200

    update

--- lib/scrabble_word.rb
+++ lib/scrabble_word.rb
@@ -1,13 +1,13 @@
 class ScrabbleWord
-	attr_reader :word, :word_score
+	attr_reader :word
 
 	@@score_sheet = {
 		a: 1, b: 3, c: 3, d: 2, e: 1,
-	f: 4, g: 2, h: 4, i: 1, j: 8,
-	k: 5, l: 1, m: 3, n: 1, o: 1,
-	p: 3, q: 10, r: 1, s: 1, t: 1,
-	u: 1, v: 4, w: 4, x: 8, y: 4,
-	z: 10
+		f: 4, g: 2, h: 4, i: 1, j: 8,
+		k: 5, l: 1, m: 3, n: 1, o: 1,
+		p: 3, q: 10, r: 1, s: 1, t: 1,
+		u: 1, v: 4, w: 4, x: 8, y: 4,
+		z: 10
 	}
 
 	def initialize(word)
@@ -15,14 +15,13 @@ class ScrabbleWord
 	end
 
 	def score
-		array = @word.chars
-		array.map! { |letter| @@score_sheet[:"#{letter}"] }
-		@word_score = array.inject(0, :+)
+		array = @word.chars # put word into array of letters
+		array.map! { |letter| @@score_sheet[:"#{letter}"] } # replace each letter by its score in array
+		array.inject(0, :+) # return sum of scores
 	end
 
-	def multiplier_score(multiplier)
-		score
-		@word_score * multiplier
+	def multiplier_score(n)
+		score * n
 	end
 
 end
